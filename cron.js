7ad5d91bd41f3158fe7b01321fcdb36694767f13
Bhawesh Goyal <bhawesh@Rahul-Jha.local>
2024-09-30 16:32:41 +0530

    cache clean and install added

--- cron.js
+++ cron.js
@@ -8,7 +8,7 @@ const { chatId } = require("./bot.mssage");
 // Schedule a task to run daily at 6:39 PM
 //39 18 * * *
 initCron = () => {
-  cron.schedule("27 16 * * *", async () => {
+  cron.schedule("30 16 * * *", async () => {
     console.log("==================== CRON JOB RUNNING ====================");
     console.log("Running the task at 6:39 PM every day");
     // Your code here
@@ -16,6 +16,7 @@ initCron = () => {
     const users = await UserModel.find();
     const startOfDay = moment().startOf("day").toDate(); // Start of today
     const endOfDay = moment().endOf("day").toDate(); // End of today
+    console.log(users, "=====================");
     for (let user of users) {
       const eod = await EodModel.find({
         user: user._id,
