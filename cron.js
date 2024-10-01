2cc2c4029f039413d76761ba404e63265d107575
Satyam Sen <satyamsen@Satyams-MacBook-Pro.local>
2024-10-01 10:50:01 +0530

    remove cron

--- cron.js
+++ cron.js
@@ -1,46 +1,46 @@
-const cron = require("node-cron");
-const { EodModel } = require("./database/eod.user");
-const { fetchUser } = require("./service/user.service");
-const { UserModel } = require("./database/user.model");
-const { bot } = require("./bot");
-const { chatId } = require("./bot.mssage");
+// const cron = require("node-cron");
+// const { EodModel } = require("./database/eod.user");
+// const { fetchUser } = require("./service/user.service");
+// const { UserModel } = require("./database/user.model");
+// const { bot } = require("./bot");
+// const { chatId } = require("./bot.mssage");
 
-// Schedule a task to run daily at 6:39 PM
-const initCron = () => {
-  cron.schedule("39 18 * * **", async () => {
-    console.log("==================== CRON JOB RUNNING ====================");
-    console.log("Running the task at 6:39 PM every day");
-    // Your code here
-    let resultPop = [];
-    const users = await UserModel.find();
-    const startOfDay = moment().startOf("day").toDate(); // Start of today
-    const endOfDay = moment().endOf("day").toDate(); // End of today
-    console.log(users, "=====================");
-    for (let user of users) {
-      const eod = await EodModel.find({
-        user: user._id,
-        createdAt: { $gte: startOfDay, $lt: endOfDay },
-      });
-      if (eod.length == 0) {
-        resultPop.push(user);
-      }
-    }
-    console.log({ resultPop }, "=====================");
-    for (let user of resultPop) {
-      bot.sendMessage(
-        chatId,
-        `User ${user.name} has not submitted EOD for today \n 🤘🤘Chin Tapak dam dam🤘🤘.... \n @${user.userName}`
-      );
-    }
-  });
+// // Schedule a task to run daily at 6:39 PM
+// const initCron = () => {
+//   cron.schedule("39 18 * * **", async () => {
+//     console.log("==================== CRON JOB RUNNING ====================");
+//     console.log("Running the task at 6:39 PM every day");
+//     // Your code here
+//     let resultPop = [];
+//     const users = await UserModel.find();
+//     const startOfDay = moment().startOf("day").toDate(); // Start of today
+//     const endOfDay = moment().endOf("day").toDate(); // End of today
+//     console.log(users, "=====================");
+//     for (let user of users) {
+//       const eod = await EodModel.find({
+//         user: user._id,
+//         createdAt: { $gte: startOfDay, $lt: endOfDay },
+//       });
+//       if (eod.length == 0) {
+//         resultPop.push(user);
+//       }
+//     }
+//     console.log({ resultPop }, "=====================");
+//     for (let user of resultPop) {
+//       bot.sendMessage(
+//         chatId,
+//         `User ${user.name} has not submitted EOD for today \n 🤘🤘Chin Tapak dam dam🤘🤘.... \n @${user.userName}`
+//       );
+//     }
+//   });
 
-  cron.schedule("* * * * *", async () => {
-    try {
-      console.log("Crom initiated");
-    } catch (error) {
-      console.log(error.message);
-    }
-  });
-};
+//   cron.schedule("* * * * *", async () => {
+//     try {
+//       console.log("Crom initiated");
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   });
+// };
 
-module.exports = { initCron };
+// module.exports = { initCron };
--- index.js
+++ index.js
@@ -60,12 +60,12 @@
 // });
 
 // Import required modules
-const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
-const { initBot } = require("./bot");
-const { connectMongoose } = require("./database");
-const { initCron } = require("./cron");
-require("dotenv").config();
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const { initBot } = require('./bot');
+const { connectMongoose } = require('./database');
+// const { initCron } = require('./cron');
+require('dotenv').config();
 // Initialize express app
 const app = express();
 
@@ -75,7 +75,7 @@ app.use(express.json());
 // Get the bot token from environment variables
 const token = process.env.TELEGRAM_TOKEN;
 if (!token) {
-  throw new Error("TELEGRAM_TOKEN must be provided!");
+  throw new Error('TELEGRAM_TOKEN must be provided!');
 }
 
 // Create a new Telegram bot instance with polling enabled
@@ -84,14 +84,14 @@ if (!token) {
 const channelId = process.env.CHANNEL_ID; // Use the channel ID from .env file, or hardcode it like -1001234567890
 
 // Route for health check (optional)
-app.get("/", (req, res) => {
-  res.send("Hello! Bot is running.");
+app.get('/', (req, res) => {
+  res.send('Hello! Bot is running.');
 });
 
 // Start the express server for the health check endpoint
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  initCron();
+  // initCron();
   initBot();
   connectMongoose();
   console.log(`Server is running on port ${PORT}`);
